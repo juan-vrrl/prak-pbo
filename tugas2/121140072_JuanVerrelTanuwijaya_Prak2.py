7d0a97e3378bc1a4a04d3309c8d698d05f630699
JuanVerrel <126883141+JuanVerrel@users.noreply.github.com>
2023-03-08 12:51:42 +0700

    Update 121140072_JuanVerrelTanuwijaya_Prak2.py

--- tugas2/121140072_JuanVerrelTanuwijaya_Prak2.py
+++ tugas2/121140072_JuanVerrelTanuwijaya_Prak2.py
@@ -1,6 +1,7 @@
 #Juan Verrel Tanuwijaya, 121140072
 #Prak 2
 
+#Inisiasi CLass Mahasiswa dan Atribut
 class Mahasiswa:    
     def __init__(self, nama, nim, kelas_pbo_siakad,jumlah_sks):
         self.nama=nama
@@ -8,12 +9,16 @@ class Mahasiswa:
         self.kelas=kelas_pbo_siakad
         self.sks=jumlah_sks
     
+    #Fungsi untuk menampilakn informasi diri dari objek berupa nama, nim, kelaspbo, dan sks
     def infodiri(self):
         print(f"Nama = {self.nama}\nNim = {self.nim}\nKelas PBO Siakad = {self.kelas}\nJumlah SKS = {self.sks}")
         
+    #Fungsi untuk mengubah kelas PBO dari objek (Ditentukan User
     def ubahkelas(self,kelasbaru):
         self.kelas=kelasbaru
 
+#Main
+#Pemanggilan class & Pemanggilan Method
 manusia=Mahasiswa("Juan", "121140072", "RB", "4 SKS")
 manusia.infodiri()
 manusia.ubahkelas("RA")
